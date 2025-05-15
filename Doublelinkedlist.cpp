6ae9c8750fc3052f9afa0dd4c899be52be6afb9c
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-15 10:59:09 +0700

    add step 2

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -146,6 +146,12 @@ class DoubleLinkedList{
                 currentNode = currentNode->next;
                 i++;
             }
+
+            // Step 2 : Traverse backward
+            cout << "\nRecords in descending order of roll number are:\n";
+            while (currentNode != NULL){
+                
+            }
         }
 };
 
