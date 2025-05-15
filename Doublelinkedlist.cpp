b32654edabd4fa9f3e7cdd83156ecce538c7f95e
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-15 09:57:22 +0700

    add metod addNote step 2

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -26,6 +26,9 @@ class DoubleLinkedList{
 
             // Step 1 : Allocate memory for new node
             Node *newNode = new Node();
+
+            // Step 2 : Assign value to the data fields
+            newNode->noMhs = nim;
         }
 
 };
