4a84289c99f40ba14b38e6b4fa6b1d7146c81360
GibranSaputra11 <gibranbantul@gmail.com>
2025-05-15 10:36:26 +0700

    add step 2 hapus

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -94,6 +94,14 @@ class DoubleLinkedList{
                 cout << "Record not found" << endl;
                 return;
             }
+
+            // Step 2 : If node is at the beginning
+            if (current == START){
+                START = current->next; // Step 2a : START = START.next
+                if (START != NULL)
+                    START->prev = NULL; // Step 2b : START.prev = NULL
+            }
+            
         }
 
 };
